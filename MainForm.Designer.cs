5bc1b039a27c32f155f017fa1a998ad82a460efc
juan21 <juan.corona@alumno.udg.mx>
2013-10-22 22:11:30 -0500

    tarea de interfaces

---
+++ MainForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace solucion
+{
+	partial class MainForm
+	{
+		private System.ComponentModel.IContainer components = null;
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+		
+			// button1
+			
+			this.button1.Location = new System.Drawing.Point(80, 201);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "inicio";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			 
+			// textBox1
+			 
+			this.textBox1.Location = new System.Drawing.Point(64, 53);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			 
+			// textBox2
+			 
+			this.textBox2.Location = new System.Drawing.Point(64, 122);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.PasswordChar = '*';
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 2;
+			
+			// label1
+			 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(64, 27);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Nombre";
+			 
+			// label2
+			 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(64, 100);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(67, 19);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Password";
+			 
+			// MainForm
+			 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "solucion";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		
+		
+		
+		void TextBox1TextChanged(object sender, System.EventArgs e)
+		{
+			
+		}	
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace solucion
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			
+		}
+		void Button1Click(object sender, System.EventArgs e)
+		{
+			
+			if(textBox1.Text=="juan" && textBox2.Text =="hola")
+			{
+				MessageBox.Show("Bienvenido juan");
+			}
+			else {
+				MessageBox.Show("No es valido");
+			}
+		}
+	}
+}
